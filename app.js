859480611e6168a11ca24e152abcf023d120aa11
taikifujino0511 <taiki.fujino@giztech-pro.com>
2026-02-04 17:44:14 +0900

    Q4の回答を記載

--- app.js
+++ app.js
@@ -15,3 +15,23 @@ let user = {
   favorite: 'card',
 };
 console.log(user.age);
+
+// Q4
+let playerList = [
+  {
+    name: 'John',
+    age: 26,
+    favorites: ['Card Game', 'Basket Ball', 'Programming'],
+  },
+  {
+    name: 'Bob',
+    age: 33,
+    favorites: ['Tinder', 'The Legend of Zelda'],
+  },
+  {
+    name: 'Michael',
+    age: 22,
+    favorites: ['Football', 'Smash Bros.'],
+  },
+];
+console.log(playerList[1].favorites[1]);
